401861663170d360c03395b598d0ecbd080ee4b2
VadimErsh <vadimersh2022@gmail.com>
2023-02-27 23:55:19 +0300

    end test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for(let i = 0; i < str.length; i++) {
+    bracketsConfig.forEach( el => {
+      if(str[i] === el[0] && str[i + 1] === el[1]) {
+        str = str.slice(0, i) + str.slice(i + 2, str.length)
+        i -= 2
+      }
+    })
+  }
+
+  return str.length === 0 ? true : false
 }
